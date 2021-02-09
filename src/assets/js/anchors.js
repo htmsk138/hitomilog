25508b5a0a366539054897cd6f50a3a6ad3adcd1
Hitomi Seki <>
2021-02-10 00:00:00 +0900

    Change anchor.js

--- src/assets/js/anchors.js
+++ src/assets/js/anchors.js
@@ -2,37 +2,36 @@ document.addEventListener('DOMContentLoaded', function() {
   /**
    * Auto-add id and anchor link to headings
    */
-  var headings = document.querySelectorAll('h2, h3, h4');
-  if (headings.length) {
-    var h2Count = 0, h3Count = 0, h4Count = 0, i = 0, max = headings.length;
-    for (; i < max; i++) {
-      switch (headings[i].tagName) {
-        case 'H2':
-          h2Count++;
-          h3Count = 0;
-          h4Count = 0;
-          break;
+  var headingIdBase = 'section';
+  var headingTags = ['h2', 'h3', 'h4', 'h5', 'h6'];
+  var headingCounts = {};
+  var i;
+  for (i = 0; i < headingTags.length; i++) {
+    headingCounts[headingTags[i]] = 0;
+  }
 
-        case 'H3':
-          h3Count++;
-          h4Count = 0;
-          break;
+  var headingElements = document.querySelectorAll(headingTags.join());
+  if (headingElements.length) {
+    for (i = 0; i < headingElements.length; i++) {
+      // Increment current heading level count
+      var tagName = headingElements[i].tagName.toLowerCase();
+      headingCounts[tagName]++;
 
-        case 'H4':
-          h4Count++;
-          break;
+      // Reset counts for headings of lower level than current
+      var headingLevel = parseInt(tagName.substring(1,2));
+      while (headingCounts.hasOwnProperty(`h${++headingLevel}`)) {
+        headingCounts[`h${headingLevel}`] = 0;
       }
-      var headingId =
-        'section' + h2Count +
-        (h3Count ? '-' + h3Count : '') +
-        (h4Count ? '-' + h4Count : '');
-      headings[i].id = headingId;
 
+      // Set ID to the heading
+      var headingId = headingIdBase + Object.values(headingCounts).filter(val => 0 !== val).join('-');
+      headingElements[i].id = headingId;
+
+      // Add anchor link next to the heading
       var link = document.createElement('a');
-      link.href = window.location.href.split('#')[0] + '#' + headingId;
-      link.title = 'ja' == document.documentElement.lang ?
-        'このセクションへのリンク' : 'Link to this section';
-      headings[i].appendChild(link);
+      link.href = `#${headingId}`;
+      link.title = 'ja' == document.documentElement.lang ? 'このセクションへのリンク' : 'Link to this section';
+      headingElements[i].appendChild(link);
     }
   }
 });
